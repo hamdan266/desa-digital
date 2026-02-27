a78dae506a4b2e174809b807d98808ee5352d4d7
Hamdan Junaedi <hamdan.work26@gmail.com>
2026-02-28 01:15:05 +0700

    create model dan migration kepala keluarga

--- database/migrations/2026_02_27_174508_create_head_of_families_table.php
+++ database/migrations/2026_02_27_174508_create_head_of_families_table.php
@@ -18,8 +18,14 @@ return new class extends Migration
             $table->foreign('user_id')->references('id')->on('users');
 
             $table->string('profile_picture');
-            
+            $table->integer('identity_number');
+            $table->enum('gender', ['male', 'female']);
+            $table->date('date_of_birth');
+            $table->string('phone_number');
+            $table->string('occupation');
+            $table->enum('marital_status', ['single', 'married']);
 
+            $table->softDeletes();
             $table->timestamps();
         });
     }
