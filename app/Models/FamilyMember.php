b74ee4c96ce1926da8ee7ed78ead12fd2199cba7
Hamdan Junaedi <hamdan.work26@gmail.com>
2026-03-01 21:54:55 +0700

    add FamilyMember model, migration, and relationships to HeadOfFamily and User models

--- app/Models/FamilyMember.php
+++ app/Models/FamilyMember.php
@@ -6,11 +6,13 @@ use App\Traits\UUID;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class HeadOfFamily extends Model
+class FamilyMember extends Model
 {
     use SoftDeletes, UUID;
 
+
     protected $fillable = [
+        'head_of_family_id',
         'user_id',
         'profile_picture',
         'identity_number',
@@ -21,13 +23,13 @@ class HeadOfFamily extends Model
         'marital_status',
     ];
 
+    public function headOfFamily()
+    {
+        return $this->belongsTo(HeadOfFamily::class);
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function familyMembers()
-    {
-        return $this->hasMany(FamilyMember::class);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(HeadOfFamily::class);
     }
+
+    public function familyMember()
+    {
+        return $this->hasOne(FamilyMember::class);
+    }
 }
